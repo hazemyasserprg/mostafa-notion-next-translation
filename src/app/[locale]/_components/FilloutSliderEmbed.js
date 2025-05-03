882c62343ee8890e389553d4dc800b7c776d8bdb
Hazem <hazemyasser911@gmail.com>
2025-05-03 16:23:11 +0300

    Arabic Form

--- src/app/[locale]/_components/FilloutSliderEmbed.js
+++ src/app/[locale]/_components/FilloutSliderEmbed.js
@@ -3,9 +3,14 @@
 import { FilloutSliderEmbed } from "@fillout/react";
 import "@fillout/react/style.css";
 import { useState } from "react";
+import { useLocale } from "next-intl"; // assuming you're using next-intl
 
 function FilloutSlider({ children, className }) {
   const [isOpen, setIsOpen] = useState(false);
+  const locale = useLocale(); // get current locale
+
+  // Determine which form ID to use based on the locale
+  const filloutId = locale === "ar" ? "d5CcFiBZsvus" : "6VeTu9jHKhus";
 
   return (
     <>
@@ -13,7 +18,7 @@ function FilloutSlider({ children, className }) {
         {children}
       </button>
       <FilloutSliderEmbed
-        filloutId="6VeTu9jHKhus"
+        filloutId={filloutId}
         inheritParameters
         parameters={{
           example: "abc",
