3cee5a841bbfb3852d365b30e6432dd08de3119b
Hazem <hazemyasser911@gmail.com>
2025-05-04 21:34:09 +0300

    Button Update

--- src/app/[locale]/templates/[slug]/TemplateDetailsClient.js
+++ src/app/[locale]/templates/[slug]/TemplateDetailsClient.js
@@ -56,10 +56,10 @@ export default function TemplateDetailsClient({ template }) {
                   href={t(`${template.name}.checkoutLink`)}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="relative inline-block overflow-hidden px-6 py-3 font-medium text-white bg-black rounded-full group shadow-secondary"
+                  className="relative inline-block overflow-hidden px-6 py-3 font-medium text-black bg-white rounded-full group shadow-lg hover:shadow-gray-300/50 hover:scale-105 transition-all duration-500 ease-in-out"
                 >
-                  <span className="absolute top-0 left-0 w-full h-full bg-white transform scale-x-0 group-hover:scale-x-100 transition-all duration-500 ease-in-out origin-left" />
-                  <span className="relative block text-lg transform transition-all duration-300 ease-in-out group-hover:text-main">
+                  <span className="absolute top-0 left-0 w-full h-full bg-black transform scale-x-0 group-hover:scale-x-100 transition-all duration-500 ease-in-out origin-left" />
+                  <span className="relative block text-lg transform transition-all duration-300 ease-in-out group-hover:text-white hover:bg-black">
                     {t(`${template.name}.checkout`)}
                   </span>
                 </a>
