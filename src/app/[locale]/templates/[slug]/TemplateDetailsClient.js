c80225e0f2c345cc3e9c9df50ddc2de3c359b476
Hazem <hazemyasser911@gmail.com>
2025-05-05 20:13:57 +0300

    Button Update

--- src/app/[locale]/templates/[slug]/TemplateDetailsClient.js
+++ src/app/[locale]/templates/[slug]/TemplateDetailsClient.js
@@ -59,7 +59,7 @@ export default function TemplateDetailsClient({ template }) {
                   className="relative inline-block overflow-hidden px-6 py-3 font-medium text-black bg-white rounded-full group shadow-lg hover:shadow-gray-300/50 hover:scale-105 transition-all duration-500 ease-in-out"
                 >
                   <span className="absolute top-0 left-0 w-full h-full bg-black transform scale-x-0 group-hover:scale-x-100 transition-all duration-500 ease-in-out origin-left" />
-                  <span className="relative block text-lg transform transition-all duration-300 ease-in-out group-hover:text-white hover:bg-black">
+                  <span className="relative block text-lg transform transition-all duration-300 ease-in-out group-hover:text-white">
                     {t(`${template.name}.checkout`)}
                   </span>
                 </a>
