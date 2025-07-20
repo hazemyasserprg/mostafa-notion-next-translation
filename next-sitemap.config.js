780e85826490863e47f1b9d3f7cd1340adf07d1b
Hazem <hazemyasser911@gmail.com>
2025-07-20 17:56:51 +0300

    fixing sitemap code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.mostafayasser.com",
   generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 5000,
   changefreq: "weekly",
   priority: 0.7,
 };
