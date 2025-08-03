ac579f899ac11a9a2bb0a54d1fce4052d41cc3be
Hazem <hazemyasser911@gmail.com>
2025-08-03 21:43:11 +0300

    fixing icon

--- src/app/[locale]/templates/[slug]/page.js
+++ src/app/[locale]/templates/[slug]/page.js
@@ -32,7 +32,7 @@ export async function generateMetadata({ params }) {
     : `https://mostafayasser.com${rawImage}`;
 
   // ✅ Determine favicon per premium template
-  let faviconPath = "/icon.webp"; // default favicon
+  let faviconPath = "/logos/icon.webp"; // default favicon
 
   if (slug === "study-hub") {
     faviconPath = "/favicons/study-hub.webp";
