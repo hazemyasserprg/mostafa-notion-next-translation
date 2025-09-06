1c76f7fec254669add04e94d0fef1fcc60298c3f
Hazem <hazemyasser911@gmail.com>
2025-09-06 20:34:59 +0300

    Update next.config.mjs and clean up files

--- next.config.mjs
+++ next.config.mjs
@@ -75,8 +75,8 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://www.notion.so https://notion.so;",
           },
           {
             key: 'X-Content-Type-Options',
